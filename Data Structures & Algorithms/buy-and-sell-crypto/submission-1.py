71885151a22b51f6bada0f41403dae43644a570a
Charan Kadamati <sricharankadamati@gmail.com>
2026-08-06 02:16:01 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxProfit(self, prices):
+        i = 0 
+        j = 0
+        max_profit = 0
+
+        while j < len(prices):
+            if prices[j] - prices[i] > max_profit:
+                max_profit = prices[j] - prices[i]
+
+            if prices[j] < prices[i]:
+                i = j 
+
+            j +=1
+        return max_profit
+
+
+
+            
+
+
